5aada480a8b7857001eb1706911090d3c731fcfb
Luc Secouard <14235161+lscrd@users.noreply.github.com>
2025-12-03 09:59:06 +0100

    Add files via upload

---
+++ Day 02/p02.nim
@@ -0,0 +1,57 @@
+import std/[strutils, strscans]
+
+type Digit = 0..9
+
+let ranges = readFile("p02.data").split(',')
+var result = 0
+
+### Part 1 ###
+
+proc isInvalid1(id: int): bool =
+  ## Return true if "id" is invalid according to part 1 rule.
+  let s = $(id)
+  if (s.len and 1) != 0: return  # Odd length => valid.
+  let m = s.len shr 1
+  result = true
+  # Check without copying.
+  for i in 0..<m:
+    if s[i] != s[i + m]:
+      return false
+
+for rng in ranges:
+  var first, last: int
+  if scanf(rng, "$i-$i", first, last):
+    for id in first..last:
+      if id.isInvalid1:
+        result += id
+echo "Part 1: ", result
+
+
+### Part 2 ###
+
+# Possible lengths for subsequences.
+const Lengths = [2: @[1], 3: @[1], 4: @[1, 2], 5: @[1], 6: @[1, 2, 3],
+                 7: @[1], 8: @[1, 2, 4], 9: @[1, 3], 10: @[1, 2, 5]]
+
+
+proc isInvalid2(id: int): bool =
+  ## Return true if "id" is invalid according to part 2 rule.
+  if id < 11: return    # First invalid id is 11.
+  let s = $(id)
+  for lg in Lengths[s.len]:
+    block Check:
+      # Check without copying.
+      for i in 0..<lg:
+        for n in countup(lg, s.len - lg, lg):
+          if s[i + n] != s[i]:
+            break Check   # Valid.
+      return true
+
+result = 0
+for rng in ranges:
+  var first, last: int
+  if scanf(rng, "$i-$i", first, last):
+    for id in first..last:
+      if id.isInvalid2:
+        result += id
+echo "Part 2: ", result
